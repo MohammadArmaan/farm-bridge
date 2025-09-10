916249a4e289ef254584a85056e56c9f8353e5e4
Mohammad Armaan <mohammadarmaan7786@Mohammads-Air.lan>
2025-09-10 20:30:58 +0530

    Mobile Nav fixed

--- client/components/MobileThemeToggler.tsx
+++ client/components/MobileThemeToggler.tsx
@@ -18,11 +18,15 @@ export function MobileThemeToggler() {
             <DropdownMenuTrigger asChild>
                 <Button variant="outline">
                     <span>Change Theme:</span>
-                    <div className="flex items-center gap-2">
-                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                        <span className="capitalize">{theme}</span>
-                    </div>
+
+                    <span className="capitalize flex items-center gap-2">
+                        {theme}{" "}
+                        {theme === "light" ? (
+                            <Sun className="h-[1.2rem] w-[1.2rem]" />
+                        ) : (
+                            <Moon className="h-[1.2rem] w-[1.2rem]" />
+                        )}
+                    </span>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="center">
